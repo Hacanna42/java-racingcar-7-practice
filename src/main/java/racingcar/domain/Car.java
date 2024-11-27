d446cd12b73fecb93affce4cc4d3041a4def362c
Hacanna42 <dtmdgus901@gmail.com>
2024-11-27 22:30:42 +0900

    feat(Cars): 자동차 객체에서 우승자 색출 관련 기능 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,5 +1,8 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import racingcar.dto.CarStatusDto;
+
 public class Car {
     private final String name;
     private final int position;
@@ -15,7 +18,30 @@ public class Car {
     }
 
     public Car move() {
-        return new Car(this.name, this.position + 1);
+        if (canMove()) {
+            return new Car(this.name, this.position + 1);
+        }
+        return this;
+    }
+
+    public boolean isAheadOf(Car other) {
+        return this.position > other.position;
+    }
+
+    public boolean isSamePositionWith(Car other) {
+        return this.position == other.position;
+    }
+
+    public CarStatusDto getCarStatus() {
+        return new CarStatusDto(name, position);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    private boolean canMove() {
+        return Randoms.pickNumberInRange(0, 9) >= 4;
     }
 
     private static void validate(String name) {
--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -2,6 +2,9 @@ package racingcar.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import racingcar.dto.CarStatusDtos;
+import racingcar.dto.WinnerCarsDto;
 
 public class Cars {
     private final List<Car> cars;
@@ -21,5 +24,32 @@ public class Cars {
     public Cars moveAllCar() {
         List<Car> movedCars = cars.stream().map(Car::move).toList();
         return new Cars(movedCars);
+    }
+
+    public CarStatusDtos getCarStatuses() {
+        return new CarStatusDtos(cars.stream()
+                .map(Car::getCarStatus)
+                .toList());
+    }
+
+    public WinnerCarsDto getWinnerCars() {
+        Car firstPlaceCar = getFirstPlaceCar();
+        List<String> winnerCarNames = cars.stream()
+                .filter(firstPlaceCar::isSamePositionWith)
+                .map(Car::getName)
+                .toList();
+
+        return new WinnerCarsDto(winnerCarNames);
+    }
+
+    private Car getFirstPlaceCar() {
+        return cars.stream().reduce(cars.getFirst(), this::chooseAheadCar);
+    }
+
+    private Car chooseAheadCar(Car car1, Car car2) {
+        if (car1.isAheadOf(car2)) {
+            return car1;
+        }
+        return car2;
     }
 }
